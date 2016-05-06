54ec9fe5ba4391d9d0f5f96f24c1dd1977927203
Basem Aljedai <baljedai@gmail.com>
2016-05-06 18:25:01 +0300

    Add main metod to Testing Class ++ Refactoring Moyasar Main Class to be MoyasarClint

--- src/com/moyasar/main/MoyasarClient.java
+++ src/com/moyasar/main/MoyasarClient.java
@@ -1,11 +1,12 @@
 package com.moyasar.main;
 
 import java.util.ArrayList;
+import java.util.UUID;
 
 import com.moyasar.bean.PaymentRequestBean;
 import com.moyasar.bean.PaymentResponseBean;
 
-public class Moyasar {
+public class MoyasarClint {
 	
 	
 	
@@ -15,7 +16,7 @@ public class Moyasar {
 	}
 	
 	
-	public PaymentResponseBean getPayment(int paymentId){
+	public PaymentResponseBean getPayment(UUID paymentId){
 		return null;
 	}
 	
--- src/com/moyasar/test/HelloMoyasar.java
+++ src/com/moyasar/test/HelloMoyasar.java
@@ -1,7 +1,28 @@
 package com.moyasar.test;
 
+import com.moyasar.bean.SadadPaymentRequestBean;
+
 public class HelloMoyasar {
 
 	
+	
+	
+	public static void main(String[] args) {
+		SadadPaymentRequestBean sqr = new SadadPaymentRequestBean();
+		sqr.setAmount(120);
+		sqr.setUsername("basem");
+		sqr.setFail_url("https://dashboard.moyasar.com/sadad/fail");
+		sqr.setSuccess_url("https://dashboard.moyasar.com/sadad/success");
+		sqr.setCurrency("SAR");
+		sqr.setDescription("Testing Java API Wrapprer for Moyasar");
+		
+		
+		System.out.println("STARTING MOYASAR JAVA API TESTING \n\n");
+		
+		System.out.println(sqr.toString());
+		
+		System.out.println("THE END OF MOYASAR JAVA API TESTING \n\n");
+		
+	}
 	
 }
